4dbed81cef201a2835b90126be7a9d613ac5703d
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-01-06 10:04:14 +0000

    add-cluster: use OpenCV to load images
    Add a dependency on OpenCV, initially for loading images but we'll be
    using it for a lot of things.
    
    Add 'meta/install-opencv-python.sh' for installing OpenCV with apt.

--- py/mel/cmd/addcluster.py
+++ py/mel/cmd/addcluster.py
@@ -4,21 +4,27 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import cv2
+
 
 def setup_parser(parser):
 
     parser.add_argument(
-        'context-filename',
+        'context',
         type=str,
         default=None,
         help="Path to the context image to add.")
 
     parser.add_argument(
-        'detail-filename',
+        'detail',
         type=str,
         default=None,
         help="Path to the detail image to add.")
 
 
 def process_args(args):
+    context_image = cv2.imread(args.context)
+    detail_image = cv2.imread(args.detail)
+    print('{}: {}'.format(args.context, context_image.shape))
+    print('{}: {}'.format(args.detail, detail_image.shape))
     raise NotImplementedError()
