68a0a31ac4265be5f0d32ba5b289479e5cfcd3f9
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-01-26 16:53:21 +0000

    micro-record: record multiple movies automatically
    Try to record one mole per movie, automatically start and stop recording
    by using the MoleAcquirer to tell us when we're looking at a new mole
    and when we've stopped looking at a mole.
    
    Add an argument for prefixing the movie files, so if we're recording
    multiple movies of the same subject then it's easy to keep them
    separated later.
    
    Test Plan:
    $ ./bin/mel micro-record --filename-prefix myprefix
    .. record multiple movies, look ok ..

--- py/mel/cmd/microrecord.py
+++ py/mel/cmd/microrecord.py
@@ -6,12 +6,22 @@ from __future__ import print_function
 
 import cv2
 
+import mel.lib.moleimaging
+
 
 def setup_parser(parser):
-    pass
+    parser.add_argument(
+        '--filename-prefix',
+        type=str,
+        default=None,
+        help="A prefix to add to each video recorded.")
 
 
 def process_args(args):
+
+    filename_format = 'mole_{number}.avi'
+    if args.filename_prefix:
+        filename_format = args.filename_prefix + '_' + filename_format
 
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
@@ -32,15 +42,11 @@ def process_args(args):
     window_height = frame_height
     cv2.resizeWindow(window_name, window_width, window_height)
 
-    # Define the codec and create VideoWriter object
-    fourcc = cv2.cv.CV_FOURCC(*'XVID')
-    out = cv2.VideoWriter(
-        'mole.avi',
-        fourcc,
-        25.0,
-        (frame_width, frame_height))
+    video_writer = None
 
     is_finished = False
+    mole_acquirer = mel.lib.moleimaging.MoleAcquirer()
+    mole_counter = 0
     while not is_finished:
         key = cv2.waitKey(50)
         if key != -1:
@@ -50,5 +56,31 @@ def process_args(args):
         if not ret:
             raise Exception("Could not read frame.")
 
-        cv2.imshow(window_name, frame)
-        out.write(frame)
+        ringed, stats = mel.lib.moleimaging.find_mole(frame)
+        mole_acquirer.update(stats)
+
+        if mole_acquirer.just_locked():
+            mole_counter += 1
+            filename = filename_format.format(number=mole_counter)
+            print(filename)
+            video_writer = make_recorder(filename, frame_width, frame_height)
+        elif mole_acquirer.just_unlocked():
+            video_writer.release()
+            video_writer = None
+
+        if mole_acquirer.is_locked:
+            # show the image with mole encircled
+            cv2.imshow(window_name, ringed)
+            video_writer.write(frame)
+        else:
+            # show the output from the microscope
+            cv2.imshow(window_name, frame)
+
+
+def make_recorder(name, width, height):
+    fourcc = cv2.cv.CV_FOURCC(*'MJPG')
+    return cv2.VideoWriter(
+        name,
+        fourcc,
+        25.0,
+        (width, height))
