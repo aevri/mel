bb3fbc1d87b21e0d108829c576f5d96a2754b703
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-01-26 13:33:48 +0000

    micro-view: print out some mole stats
    Start trying to identify stats which might enable automatic
    identification of moles.
    
    If we can automatically identify a mole then the
    'search for new and changing moles' workflow could be made faster.
    
    Test Plan:
    $ ./bin/mel micro-view
    .. observe vaguely stable stats printed out ..

--- py/mel/cmd/microview.py
+++ py/mel/cmd/microview.py
@@ -4,6 +4,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import math
+
 import cv2
 
 
@@ -27,6 +29,8 @@ def process_args(args):
     # loop until the user presses a key
     print("Press any key to exit.")
     is_finished = False
+    last_stats = None
+    last_stats_diff = None
     while not is_finished:
         key = cv2.waitKey(50)
         if key != -1:
@@ -42,10 +46,34 @@ def process_args(args):
         img = cv2.cvtColor(img, cv2.cv.CV_BGR2HSV)
         img = cv2.split(img)[1]
         _, img = cv2.threshold(img, 30, 255, cv2.cv.CV_THRESH_BINARY)
-        ringed = _process_contours(img, frame)
+        ringed, stats = _process_contours(img, frame)
+
+        if stats and last_stats:
+            stats_diff = map(lambda x, y: x - y, last_stats, stats)
+            last_stats = map(lambda x, y: _lerp(x, y, 0.1), last_stats, stats)
+
+            if last_stats_diff:
+                last_stats_diff = map(
+                    lambda x, y: _lerp(x, y, 0.1), last_stats_diff, stats_diff)
+
+                print(
+                    map(lambda x: int(x), last_stats),
+                    map(lambda x: int(x), last_stats_diff)
+                )
+            else:
+                last_stats_diff = stats_diff
+
+
+        elif stats:
+            last_stats = stats
 
         # show the image with 'non-mole' areas encircled
         cv2.imshow(window_name, ringed)
+
+
+def _lerp(origin, target, factor_0_to_1):
+    towards = target - origin
+    return origin + (towards * factor_0_to_1)
 
 
 def _find_mole_contour(contours):
@@ -58,19 +86,20 @@ def _find_mole_contour(contours):
                 mole_contour = contour
                 mole_area = area
 
-    return mole_contour
+    return mole_contour, mole_area
 
 
 def _process_contours(mole_regions, original):
 
     final = original.copy()
+    stats = None
 
     contours, hierarchy = cv2.findContours(
         mole_regions.copy(),
         cv2.cv.CV_RETR_LIST,
         cv2.cv.CV_CHAIN_APPROX_NONE)
 
-    mole_contour = _find_mole_contour(contours)
+    mole_contour, mole_area = _find_mole_contour(contours)
     if mole_contour is not None:
         if len(mole_contour) > 5:
 
@@ -82,5 +111,6 @@ def _process_contours(mole_regions, original):
             blue = (255, 0, 0)
 
             cv2.ellipse(final, ellipse, blue, 5)
+            stats = (math.sqrt(mole_area), ellipse[1][0], ellipse[1][1])
 
-    return final
+    return final, stats
