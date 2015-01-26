9e30287ff0c0a810916f86db715fe76d92518467
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-01-26 19:51:06 +0000

    micro-replay: support vector machine to classify
    Try to classify moles based on the stats we're extracting, use a
    'support vector machine', which seems to be the method suggested by the
    rather useful cheat sheet here:
    http://scikit-learn.org/stable/tutorial/machine_learning_map/
    
    With enough samples of each mole, we might be able to automatically
    determine which is which. Extensive testing required.
    
    Test Plan:
    $ ./bin/mel micro-replay *.avi
    .. successfully predicts first and last mole, so far so good ..

--- py/mel/cmd/microreplay.py
+++ py/mel/cmd/microreplay.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 import multiprocessing
 
 import cv2
+import numpy
 
 import mel.lib.moleimaging
 
@@ -21,13 +22,33 @@ def setup_parser(parser):
 
 def process_args(args):
 
-    pool = multiprocessing.Pool()
+    # _test_classifier()
+    # _test_named_classifier()
+    # return
 
+    pool = multiprocessing.Pool()
+    classifier = NamedClassifier()
+
+    name_stats = []
     filename_stats_iter = pool.imap_unordered(get_stats, args.path)
     for filename, stats in filename_stats_iter:
-        print(filename)
+        name = filename.split('_')[0]
+        print(filename, name)
         if stats:
             print(map(lambda x: int(x), stats))
+            name_stats.append((name, stats))
+
+    training_samples = name_stats[1:-1]
+    test_samples = name_stats[:1] + name_stats[-1:]
+
+    for name, stats in training_samples:
+        classifier.add_sample(stats, name)
+
+    classifier.train()
+
+    for name, stats in test_samples:
+        predicted_name = classifier.predict(stats)
+        print(name, stats, '->', predicted_name)
 
 
 def get_stats(filename):
@@ -52,3 +73,113 @@ def get_stats(filename):
             final_stats = stats
 
     return filename, final_stats
+
+
+class NamedClassifier(object):
+
+    def __init__(self):
+        super(NamedClassifier, self).__init__()
+        self._classifier = Classifier()
+        self._name_to_number = {}
+        self._number_to_name = {}
+        self._next_class_num = 0
+
+    def add_sample(self, data, class_name):
+
+        if class_name not in self._name_to_number:
+            class_num = self._next_class_num
+            self._name_to_number[class_name] = class_num
+            self._number_to_name[class_num] = class_name
+            self._next_class_num += 1
+        else:
+            class_num = self._name_to_number[class_name]
+
+        self._classifier.add_sample(data, class_num)
+
+    def train(self):
+        self._classifier.train()
+
+    def predict(self, data):
+        class_num = self._classifier.predict(data)
+        return self._number_to_name[int(class_num)]
+
+
+class Classifier(object):
+
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self._training_data = []
+        self._responses = []
+        self._svm = cv2.SVM()
+
+    def add_sample(self, data, class_num):
+        self._training_data.append(data)
+        self._responses.append(class_num)
+
+    def train(self):
+
+        svm_params = {
+            "kernel_type": cv2.SVM_LINEAR,
+            "svm_type": cv2.SVM_C_SVC,
+            "C": 1
+        }
+
+        training_data = numpy.array(
+            [
+                numpy.array(x, dtype=numpy.float32)
+                for x in self._training_data
+            ],
+            dtype=numpy.float32)
+
+        responses = numpy.array(self._responses, dtype=numpy.float32)
+
+        self._svm.train(
+            training_data,
+            responses,
+            params=svm_params)
+
+    def predict(self, data):
+        return self._svm.predict(
+            numpy.array(data, dtype=numpy.float32))
+
+
+def _test_named_classifier():
+
+    classifier = NamedClassifier()
+
+    classifier.add_sample([0.0, 1.0], "badger")
+    classifier.add_sample([1.0, 1.0], "orange")
+    classifier.add_sample([1.0, 1.0], "orange")
+    classifier.add_sample([0.0, 1.0], "badger")
+    classifier.add_sample([1.0, 1.0], "orange")
+    classifier.add_sample([0.0, 1.0], "badger")
+    classifier.add_sample([0.5, 0.5], "tea")
+    classifier.add_sample([0.5, 0.5], "tea")
+    classifier.add_sample([0.5, 0.5], "tea")
+
+    classifier.train()
+
+    print(classifier.predict([1.0, 1.0]))
+    print(classifier.predict([0.0, 1.0]))
+    print(classifier.predict([0.5, 0.5]))
+
+
+def _test_classifier():
+
+    classifier = Classifier()
+
+    classifier.add_sample([0.0, 1.0], 2.0)
+    classifier.add_sample([1.0, 1.0], 0.0)
+    classifier.add_sample([1.0, 1.0], 0.0)
+    classifier.add_sample([0.0, 1.0], 2.0)
+    classifier.add_sample([1.0, 1.0], 0.0)
+    classifier.add_sample([0.0, 1.0], 2.0)
+    classifier.add_sample([0.5, 0.5], 1.0)
+    classifier.add_sample([0.5, 0.5], 1.0)
+    classifier.add_sample([0.5, 0.5], 1.0)
+
+    classifier.train()
+
+    print(classifier.predict([1.0, 1.0]))
+    print(classifier.predict([0.0, 1.0]))
+    print(classifier.predict([0.5, 0.5]))
