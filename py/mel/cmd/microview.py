7b9272ad13d0fe30b50a69a0ab656c3702421b47
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-01-26 11:29:46 +0000

    micro-view: highlight likely moles
    Exploit the tendancy of moles to have higher saturation than surrounding
    skin, use this to highlight areas of high saturation as likely moles.
    
    Test Plan:
    $ ./bin/mel micro-view
    .. observe pretty good highlighting of just moles and sometimes hair ..

--- py/mel/cmd/microview.py
+++ py/mel/cmd/microview.py
@@ -16,6 +16,7 @@ def process_args(args):
     if not cap.isOpened():
         raise Exception("Could not open video capture device.")
 
+    # create an 800x600 window
     window_name = "output"
     cv2.namedWindow(window_name)
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
@@ -23,10 +24,11 @@ def process_args(args):
     window_height = 600
     cv2.resizeWindow(window_name, window_width, window_height)
 
+    # loop until the user presses a key
+    print("Press any key to exit.")
     is_finished = False
     while not is_finished:
         key = cv2.waitKey(50)
-
         if key != -1:
             raise Exception('User aborted.')
 
@@ -34,4 +36,14 @@ def process_args(args):
         if not ret:
             raise Exception("Could not read frame.")
 
-        cv2.imshow(window_name, frame)
+        # highlight areas of high saturation, they are likely moles
+        img = frame.copy()
+        img = cv2.blur(img, (40, 40))
+        img = cv2.cvtColor(img, cv2.cv.CV_BGR2HSV)
+        img = cv2.split(img)[1]
+        _, img = cv2.threshold(img, 30, 255, cv2.cv.CV_THRESH_BINARY)
+        masked = cv2.bitwise_and(frame, frame, mask=img)
+        highlighted = cv2.addWeighted(frame, 0.75, masked, 0.25, 0.0)
+
+        # show the image with 'non-mole' areas faded
+        cv2.imshow(window_name, highlighted)
