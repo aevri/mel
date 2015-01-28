aa4115d1ac8c49872239aa10c973d27aa02a0e9a
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-01-28 08:51:10 +0000

    Mel: move classification to 'mel micro-analyse'
    Move the classification code into a new command, so that it can be
    exercised without analysing videos directly.
    
    Save statistics from 'mel micro-replay' to json files, which
    'mel micro-analyse' loads.
    
    Test Plan:
    $ ./bin/mel micro-replay *.avi
    .. records .json files ..
    $ ./bin/mel micro-analyse *.json
    .. does simple classification ..

---
+++ py/mel/cmd/microanalyse.py
@@ -0,0 +1,150 @@
+"""Analyse statistics recorded about moles."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2
+import json
+import numpy
+
+
+def setup_parser(parser):
+    parser.add_argument(
+        'path',
+        type=str,
+        nargs='+',
+        help="Paths to stats to analyse.")
+
+
+def process_args(args):
+
+    name_stats = []
+    for filename in args.path:
+        with open(filename) as f:
+            stats = json.load(f)
+            for sample in stats:
+                name_stats.append((filename, sample))
+
+    classifier = NamedClassifier()
+    training_samples = name_stats[1:-1]
+    test_samples = name_stats[:1] + name_stats[-1:]
+
+    for name, stats in training_samples:
+        classifier.add_sample(stats, name)
+
+    classifier.train()
+
+    for name, stats in test_samples:
+        predicted_name = classifier.predict(stats)
+        print(name, stats, '->', predicted_name)
+
+
+class NamedClassifier(object):
+
+    def __init__(self):
+        super(NamedClassifier, self).__init__()
+        self._classifier = Classifier()
+        self._name_to_number = {}
+        self._number_to_name = {}
+        self._next_class_num = 0
+
+    def add_sample(self, data, class_name):
+
+        if class_name not in self._name_to_number:
+            class_num = self._next_class_num
+            self._name_to_number[class_name] = class_num
+            self._number_to_name[class_num] = class_name
+            self._next_class_num += 1
+        else:
+            class_num = self._name_to_number[class_name]
+
+        self._classifier.add_sample(data, class_num)
+
+    def train(self):
+        self._classifier.train()
+
+    def predict(self, data):
+        class_num = self._classifier.predict(data)
+        return self._number_to_name[int(class_num)]
+
+
+class Classifier(object):
+
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self._training_data = []
+        self._responses = []
+        self._svm = cv2.SVM()
+
+    def add_sample(self, data, class_num):
+        self._training_data.append(data)
+        self._responses.append(class_num)
+
+    def train(self):
+
+        svm_params = {
+            "kernel_type": cv2.SVM_LINEAR,
+            "svm_type": cv2.SVM_C_SVC,
+            "C": 1
+        }
+
+        training_data = numpy.array(
+            [
+                numpy.array(x, dtype=numpy.float32)
+                for x in self._training_data
+            ],
+            dtype=numpy.float32)
+
+        responses = numpy.array(self._responses, dtype=numpy.float32)
+
+        self._svm.train(
+            training_data,
+            responses,
+            params=svm_params)
+
+    def predict(self, data):
+        return self._svm.predict(
+            numpy.array(data, dtype=numpy.float32))
+
+
+def _test_named_classifier():
+
+    classifier = NamedClassifier()
+
+    classifier.add_sample([0.0, 1.0], "badger")
+    classifier.add_sample([1.0, 1.0], "orange")
+    classifier.add_sample([1.0, 1.0], "orange")
+    classifier.add_sample([0.0, 1.0], "badger")
+    classifier.add_sample([1.0, 1.0], "orange")
+    classifier.add_sample([0.0, 1.0], "badger")
+    classifier.add_sample([0.5, 0.5], "tea")
+    classifier.add_sample([0.5, 0.5], "tea")
+    classifier.add_sample([0.5, 0.5], "tea")
+
+    classifier.train()
+
+    print(classifier.predict([1.0, 1.0]))
+    print(classifier.predict([0.0, 1.0]))
+    print(classifier.predict([0.5, 0.5]))
+
+
+def _test_classifier():
+
+    classifier = Classifier()
+
+    classifier.add_sample([0.0, 1.0], 2.0)
+    classifier.add_sample([1.0, 1.0], 0.0)
+    classifier.add_sample([1.0, 1.0], 0.0)
+    classifier.add_sample([0.0, 1.0], 2.0)
+    classifier.add_sample([1.0, 1.0], 0.0)
+    classifier.add_sample([0.0, 1.0], 2.0)
+    classifier.add_sample([0.5, 0.5], 1.0)
+    classifier.add_sample([0.5, 0.5], 1.0)
+    classifier.add_sample([0.5, 0.5], 1.0)
+
+    classifier.train()
+
+    print(classifier.predict([1.0, 1.0]))
+    print(classifier.predict([0.0, 1.0]))
+    print(classifier.predict([0.5, 0.5]))
--- py/mel/cmd/microreplay.py
+++ py/mel/cmd/microreplay.py
@@ -1,4 +1,4 @@
-"""Replay recorded videos of moles and analyse."""
+"""Replay recorded videos of moles and record stats."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -6,8 +6,9 @@ from __future__ import print_function
 
 import multiprocessing
 
+import collections
 import cv2
-import numpy
+import json
 
 import mel.lib.moleimaging
 
@@ -22,33 +23,20 @@ def setup_parser(parser):
 
 def process_args(args):
 
-    # _test_classifier()
-    # _test_named_classifier()
-    # return
-
     pool = multiprocessing.Pool()
-    classifier = NamedClassifier()
 
-    name_stats = []
+    name_stats = collections.defaultdict(list)
     filename_stats_iter = pool.imap_unordered(get_stats, args.path)
     for filename, stats in filename_stats_iter:
         name = filename.split('_')[0]
         print(filename, name)
         if stats:
             print(map(lambda x: int(x), stats))
-            name_stats.append((name, stats))
+            name_stats[name].append(stats)
 
-    training_samples = name_stats[1:-1]
-    test_samples = name_stats[:1] + name_stats[-1:]
-
-    for name, stats in training_samples:
-        classifier.add_sample(stats, name)
-
-    classifier.train()
-
-    for name, stats in test_samples:
-        predicted_name = classifier.predict(stats)
-        print(name, stats, '->', predicted_name)
+    for name, stats in name_stats.iteritems():
+        with open('{}.json'.format(name), 'w') as f:
+            json.dump(stats, f, indent=4)
 
 
 def get_stats(filename):
@@ -73,113 +61,3 @@ def get_stats(filename):
             final_stats = stats
 
     return filename, final_stats
-
-
-class NamedClassifier(object):
-
-    def __init__(self):
-        super(NamedClassifier, self).__init__()
-        self._classifier = Classifier()
-        self._name_to_number = {}
-        self._number_to_name = {}
-        self._next_class_num = 0
-
-    def add_sample(self, data, class_name):
-
-        if class_name not in self._name_to_number:
-            class_num = self._next_class_num
-            self._name_to_number[class_name] = class_num
-            self._number_to_name[class_num] = class_name
-            self._next_class_num += 1
-        else:
-            class_num = self._name_to_number[class_name]
-
-        self._classifier.add_sample(data, class_num)
-
-    def train(self):
-        self._classifier.train()
-
-    def predict(self, data):
-        class_num = self._classifier.predict(data)
-        return self._number_to_name[int(class_num)]
-
-
-class Classifier(object):
-
-    def __init__(self):
-        super(Classifier, self).__init__()
-        self._training_data = []
-        self._responses = []
-        self._svm = cv2.SVM()
-
-    def add_sample(self, data, class_num):
-        self._training_data.append(data)
-        self._responses.append(class_num)
-
-    def train(self):
-
-        svm_params = {
-            "kernel_type": cv2.SVM_LINEAR,
-            "svm_type": cv2.SVM_C_SVC,
-            "C": 1
-        }
-
-        training_data = numpy.array(
-            [
-                numpy.array(x, dtype=numpy.float32)
-                for x in self._training_data
-            ],
-            dtype=numpy.float32)
-
-        responses = numpy.array(self._responses, dtype=numpy.float32)
-
-        self._svm.train(
-            training_data,
-            responses,
-            params=svm_params)
-
-    def predict(self, data):
-        return self._svm.predict(
-            numpy.array(data, dtype=numpy.float32))
-
-
-def _test_named_classifier():
-
-    classifier = NamedClassifier()
-
-    classifier.add_sample([0.0, 1.0], "badger")
-    classifier.add_sample([1.0, 1.0], "orange")
-    classifier.add_sample([1.0, 1.0], "orange")
-    classifier.add_sample([0.0, 1.0], "badger")
-    classifier.add_sample([1.0, 1.0], "orange")
-    classifier.add_sample([0.0, 1.0], "badger")
-    classifier.add_sample([0.5, 0.5], "tea")
-    classifier.add_sample([0.5, 0.5], "tea")
-    classifier.add_sample([0.5, 0.5], "tea")
-
-    classifier.train()
-
-    print(classifier.predict([1.0, 1.0]))
-    print(classifier.predict([0.0, 1.0]))
-    print(classifier.predict([0.5, 0.5]))
-
-
-def _test_classifier():
-
-    classifier = Classifier()
-
-    classifier.add_sample([0.0, 1.0], 2.0)
-    classifier.add_sample([1.0, 1.0], 0.0)
-    classifier.add_sample([1.0, 1.0], 0.0)
-    classifier.add_sample([0.0, 1.0], 2.0)
-    classifier.add_sample([1.0, 1.0], 0.0)
-    classifier.add_sample([0.0, 1.0], 2.0)
-    classifier.add_sample([0.5, 0.5], 1.0)
-    classifier.add_sample([0.5, 0.5], 1.0)
-    classifier.add_sample([0.5, 0.5], 1.0)
-
-    classifier.train()
-
-    print(classifier.predict([1.0, 1.0]))
-    print(classifier.predict([0.0, 1.0]))
-    print(classifier.predict([0.5, 0.5]))
