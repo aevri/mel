2e1f098d6837d6c25fdf5614a2f3637429459e08
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-12-28 12:43:25 +0000

    Add meta/run_tests.sh
    Now that we have multiple test scripts in the project, make it easy to
    run them all at once.
    
    Test Plan:
    
    Check that 'no error' case and 'an error' cases are handled as expected.
    
    $ ./meta/run_tests.sh
    .. ensure no errors ..
    .. introduce whitespace mistakes that autofix will fix ..
    $ ./meta/run_tests.sh
    .. ensure deliberate mistakes are corrected ..
    .. introduce syntax error that static_tests will report ..
    $ ./meta/run_tests.sh
    .. ensure deliberate mistakes are reported ..
    .. introduce behaviour error that unit_tests will find ..
    $ ./meta/run_tests.sh
    .. ensure deliberate mistakes are reported ..

---
+++ meta/run_tests.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+die() {
+    echo $@ >&2
+    exit 1
+}
+
+# cd to the root of the repository, so all the paths are relative to that
+cd "$(dirname "$0")"/.. || die 'Could not cd to repo root'
+
+# check that the working copy is clean
+git diff --exit-code
+if [ "$?" -ne 0 ]; then
+    echo
+    echo 'The working copy is dirty (see diff above)'
+    echo
+    echo 'We need a clean working copy before automatically running "autofix"'
+    echo 'otherwise it may be confusing as to where changes came from. Also '
+    echo 'we want to check for a clean working copy afterwards to make sure '
+    echo 'that autofix did not make any changes.'
+    exit 1
+fi
+
+printf "autofix: "
+./meta/autofix.sh || die "Could not run autofix."
+git diff --exit-code
+if [ "$?" -ne 0 ]; then
+    echo
+    echo The working copy is dirty after automatic fixes, please review and
+    echo add the changes before continuing.
+    exit 1
+fi
+
+printf "static tests: "
+./meta/static_tests.sh || die 'Static tests failed.'
+
+printf "unit tests: "
+./meta/unit_tests.sh || die 'Unit tests failed.'
