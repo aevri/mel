c72fcedecc933696d53938ee8eedb6aa245a49d1
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-12-31 10:11:12 +0000

    meta/unit_tests.sh: pass-through args to nosetests
    Make it easy to add arguments like '-v' when running, by passing through
    any arguments to the script.
    
    Test Plan:
    $ ./meta/unit_tests.sh -v
    .. see verbose output ..

--- meta/unit_tests.sh
+++ meta/unit_tests.sh
@@ -3,4 +3,4 @@
 # cd to the root of the repository, so all the paths are relative to that
 cd "$(dirname "$0")"/..
 
-nosetests py/ --with-doc --doctest-tests --all-modules
+nosetests py/ --with-doc --doctest-tests --all-modules $@
