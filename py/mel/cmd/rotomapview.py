22032ec69b87c2661bd94d495177ff27f1633ad2
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-12-31 14:17:06 +0000

    Add 'mel rotomap-view' command
    Introduce the idea of a 'rotomap', where a series of images is taken
    whilst a particular subject rotates clockwise, e.g. 'lower left leg'.
    
    This provides excellent context reference for moles, as you can get a
    very clear idea of where they are in relation to eachother when viewing
    from multiple angles.
    
    Provide a new command 'rotomap-view', which simply displays the images
    in a 'rotomap'.
    
    In later work we'll be able to record the locations of moles in the maps
    so that they can be counted and tracked. This should provide a way to
    monitor for new moles - by comparing which moles appear in old and new
    rotomaps.
    
    We will also have the possibility of automatically generating the
    context images that are so useful for identifying moles. This should
    work for both cluster images as well as single images.
    
    Test Plan:
    $ ./meta/run_tests.sh
    .. view a simple rotomap with 'mel rotomap-view' ..

---
+++ py/mel/cmd/rotomapview.py
@@ -0,0 +1,90 @@
+"""View a 'rotomap' series of images."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2
+import os
+
+import mel.lib.ui
+
+
+def setup_parser(parser):
+    parser.add_argument(
+        'PATH',
+        type=str,
+        help="Path to the mole to add new microscope images to.")
+    parser.add_argument(
+        '--display-width',
+        type=int,
+        default=800,
+        help="Width of the preview display window.")
+    parser.add_argument(
+        '--display-height',
+        type=int,
+        default=600,
+        help="Width of the preview display window.")
+    parser.add_argument(
+        '--rot90',
+        type=int,
+        default=None,
+        help="Rotate images 90 degrees clockwise this number of times.")
+
+
+def process_args(args):
+    display = Display(
+        args.PATH, args.display_width, args.display_height, args.rot90)
+
+    left = 63234
+    right = 63235
+
+    print("Press any other key to save and quit.")
+    is_finished = False
+    while not is_finished:
+        key = cv2.waitKey(50)
+        if key != -1:
+            if key == left:
+                display.show_prev()
+            elif key == right:
+                display.show_next()
+            else:
+                is_finished = True
+
+
+class Display:
+
+    def __init__(self, path, width, height, rot90):
+        self._display = mel.lib.ui.MultiImageDisplay(path, width, height)
+        self._rot90 = rot90
+
+        # list all images
+        self._path_list = [
+            os.path.join(path, x)
+            for x in os.listdir(path)
+        ]
+
+        self._list_index = 0
+        self._num_images = len(self._path_list)
+
+        image = self.load_current_image()
+        self._display_index = self._display.add_image(image)
+
+    def load_current_image(self):
+        image = cv2.imread(self._path_list[self._list_index])
+        if self._rot90:
+            image = mel.lib.common.rotated90(image, self._rot90)
+        return image
+
+    def show_current(self):
+        image = self.load_current_image()
+        self._display.update_image(image, self._display_index)
+
+    def show_prev(self):
+        new_index = self._list_index + self._num_images - 1
+        self._list_index = new_index % self._num_images
+        self.show_current()
+
+    def show_next(self):
+        self._list_index = (self._list_index + 1) % self._num_images
+        self.show_current()
