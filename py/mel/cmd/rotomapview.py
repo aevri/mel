befb09aebaa0bbee4f38af04b7ec7ad29d637371
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2016-01-01 18:53:47 +0000

    rotomap-view: ctrl-click to zoom
    To make it easier to see what's really a mole and what's not, add
    support for ctrl-clicking on a point to centre on it and zoom to full
    resolution.
    
    Press the space bar to return to 'fit image to display' mode.
    
    Test Plan:
    $ ./meta/run_tests.sh
    .. view a rotomap using 'mel rotomap-view' ..

--- py/mel/cmd/rotomapview.py
+++ py/mel/cmd/rotomapview.py
@@ -7,6 +7,10 @@ from __future__ import print_function
 import cv2
 import os
 
+import mel.lib.common
+import mel.lib.image
+import mel.lib.math
+
 
 def setup_parser(parser):
     parser.add_argument(
@@ -37,7 +41,17 @@ def process_args(args):
     left = 63234
     right = 63235
 
+    def mouse_callback(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if flags & cv2.EVENT_FLAG_CTRLKEY:
+                image_x, image_y = display.windowxy_to_imagexy(x, y)
+                display.show_zoomed(image_x, image_y)
+
+    display.set_mouse_callback(mouse_callback)
+
     print("Press left for previous image, right for next image.")
+    print("Ctrl-click on a point to zoom in on it.")
+    print("Press space to restore original zoom.")
     print("Press any other key to quit.")
     is_finished = False
     while not is_finished:
@@ -47,8 +61,12 @@ def process_args(args):
                 display.show_prev()
             elif key == right:
                 display.show_next()
+            elif key == ord(' '):
+                display.show_current()
             else:
                 is_finished = True
+
+    display.clear_mouse_callback()
 
 
 class Display:
@@ -82,9 +100,36 @@ class Display:
 
     def show_current(self):
         image = self.load_current_image()
+
+        self._image_width = image.shape[1]
+        self._image_height = image.shape[0]
+        letterbox = mel.lib.image.calc_letterbox(
+            self._image_width,
+            self._image_height,
+            self._width,
+            self._height)
+
+        self._image_left = letterbox[0]
+        self._image_top = letterbox[1]
+        self._image_scale = image.shape[1] / letterbox[2]
+
         image = mel.lib.image.letterbox(
             image, self._width, self._height)
         cv2.imshow(self._name, image)
+
+    def windowxy_to_imagexy(self, window_x, window_y):
+        image_x = mel.lib.math.clamp(
+            window_x - self._image_left,
+            0,
+            self._image_width)
+        image_y = mel.lib.math.clamp(
+            window_y - self._image_top,
+            0,
+            self._image_height)
+        return (
+            int(image_x * self._image_scale),
+            int(image_y * self._image_scale)
+        )
 
     def show_prev(self):
         new_index = self._list_index + self._num_images - 1
@@ -94,3 +139,28 @@ class Display:
     def show_next(self):
         self._list_index = (self._list_index + 1) % self._num_images
         self.show_current()
+
+    def show_zoomed(self, x, y):
+        image = self.load_current_image()
+        nx, ny = mel.lib.image.calc_centering_offset(
+            (x, y),
+            (image.shape[1], image.shape[0]),
+            (self._width, self._height))
+        image = mel.lib.image.translated_and_clipped(
+            image, nx, ny, self._width, self._height)
+        self._image_left = -nx
+        self._image_top = -ny
+        self._image_width = image.shape[1] + nx
+        self._image_height = image.shape[0] + ny
+        self._image_scale = 1
+        cv2.imshow(self._name, image)
+
+    def set_mouse_callback(self, callback):
+        cv2.setMouseCallback(self._name, callback)
+
+    def clear_mouse_callback(self):
+
+        def null_handler(event, x, y, flags, param):
+            pass
+
+        cv2.setMouseCallback(self._name, null_handler)
