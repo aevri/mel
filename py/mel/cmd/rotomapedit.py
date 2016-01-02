82bb91def3f3a6707a804f9d218e969c71d2f0e6
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2016-01-02 19:21:06 +0000

    rotomap-edit: click to add moles
    Left-click on the image to add a mole at that point and save to disk.
    
    Moles are automatically loaded along with an image, if present.
    
    Zooming is preserved when moving between images.
    
    Test Plan:
    $ ./meta/run_tests.sh
    .. Add many moles with left-click, using rotomap-edit. Reload. ..

--- py/mel/cmd/rotomapedit.py
+++ py/mel/cmd/rotomapedit.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 import cv2
+import json
 import os
 
 import mel.lib.common
@@ -46,10 +47,14 @@ def process_args(args):
             if flags & cv2.EVENT_FLAG_CTRLKEY:
                 image_x, image_y = display.windowxy_to_imagexy(x, y)
                 display.show_zoomed(image_x, image_y)
+            else:
+                image_x, image_y = display.windowxy_to_imagexy(x, y)
+                display.add_mole(image_x, image_y)
 
     display.set_mouse_callback(mouse_callback)
 
     print("Press left for previous image, right for next image.")
+    print("Click on a point to add a mole there and save.")
     print("Ctrl-click on a point to zoom in on it.")
     print("Press space to restore original zoom.")
     print("Press any other key to quit.")
@@ -63,7 +68,7 @@ def process_args(args):
             elif key == right:
                 display.show_next()
             elif key == ord(' '):
-                display.show_current()
+                display.show_fitted()
             else:
                 is_finished = True
 
@@ -78,10 +83,13 @@ class Display:
         self._height = height
         self._rot90 = rot90
 
+        self._moles = []
+
         # list all images
         self._path_list = [
             os.path.join(path, x)
             for x in os.listdir(path)
+            if x.endswith('.jpg')
         ]
 
         cv2.namedWindow(self._name)
@@ -91,15 +99,29 @@ class Display:
         self._list_index = 0
         self._num_images = len(self._path_list)
 
+        self._is_zoomed = False
+
         self.show_current()
 
     def load_current_image(self):
-        image = cv2.imread(self._path_list[self._list_index])
+        image_path = self._path_list[self._list_index]
+        image = cv2.imread(image_path)
+        moles_path = image_path + '.json'
+        self._moles = []
+        if os.path.exists(moles_path):
+            with open(moles_path) as moles_file:
+                self._moles = json.load(moles_file)
         if self._rot90:
             image = mel.lib.common.rotated90(image, self._rot90)
         return image
 
     def show_current(self):
+        if not self._is_zoomed:
+            self.show_fitted()
+        else:
+            self.show_zoomed(self._zoom_x, self._zoom_y)
+
+    def show_fitted(self):
         image = self.load_current_image()
 
         self._image_width = image.shape[1]
@@ -116,7 +138,38 @@ class Display:
 
         image = mel.lib.image.letterbox(
             image, self._width, self._height)
+
+        for mole in self._moles:
+            x = int(mole[0] / self._image_scale + self._image_left)
+            y = int(mole[1] / self._image_scale + self._image_top)
+            cv2.circle(image, (x, y), 10, (255, 0, 0), -1)
+
         cv2.imshow(self._name, image)
+        self._is_zoomed = False
+
+    def show_zoomed(self, x, y):
+        image = self.load_current_image()
+        nx, ny = mel.lib.image.calc_centering_offset(
+            (x, y),
+            (image.shape[1], image.shape[0]),
+            (self._width, self._height))
+        image = mel.lib.image.translated_and_clipped(
+            image, nx, ny, self._width, self._height)
+        self._zoom_x = x
+        self._zoom_y = y
+        self._image_left = -nx
+        self._image_top = -ny
+        self._image_width = image.shape[1] + nx
+        self._image_height = image.shape[0] + ny
+        self._image_scale = 1
+        for mole in self._moles:
+            x = mole[0] + self._image_left
+            y = mole[1] + self._image_top
+            if x >= 0 and y >= 0:
+                if x < self._image_width and y < self._image_height:
+                    cv2.circle(image, (x, y), 10, (255, 0, 0), -1)
+        cv2.imshow(self._name, image)
+        self._is_zoomed = True
 
     def windowxy_to_imagexy(self, window_x, window_y):
         image_x = mel.lib.math.clamp(
@@ -141,20 +194,13 @@ class Display:
         self._list_index = (self._list_index + 1) % self._num_images
         self.show_current()
 
-    def show_zoomed(self, x, y):
-        image = self.load_current_image()
-        nx, ny = mel.lib.image.calc_centering_offset(
-            (x, y),
-            (image.shape[1], image.shape[0]),
-            (self._width, self._height))
-        image = mel.lib.image.translated_and_clipped(
-            image, nx, ny, self._width, self._height)
-        self._image_left = -nx
-        self._image_top = -ny
-        self._image_width = image.shape[1] + nx
-        self._image_height = image.shape[0] + ny
-        self._image_scale = 1
-        cv2.imshow(self._name, image)
+    def add_mole(self, x, y):
+        self._moles.append((x, y))
+        image_path = self._path_list[self._list_index]
+        moles_path = image_path + '.json'
+        with open(moles_path, 'w') as moles_file:
+            json.dump(self._moles, moles_file)
+        self.show_current()
 
     def set_mouse_callback(self, callback):
         cv2.setMouseCallback(self._name, callback)
