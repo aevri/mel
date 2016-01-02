c10d171494ef9e6a7b069b803b2c9618c7f99034
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2016-01-02 12:18:45 +0000

    mel.lib.math: Add 'clamp' helper function
    Add a helper function for clamping values between two inclusive limits,
    this is often useful for dealing with input from users.
    
    Test Plan: $ ./meta/run_tests.sh

--- py/mel/lib/math.py
+++ py/mel/lib/math.py
@@ -8,3 +8,20 @@ from __future__ import print_function
 def lerp(origin, target, factor_0_to_1):
     towards = target - origin
     return origin + (towards * factor_0_to_1)
+
+
+def clamp(x, x_min, x_max):
+    """Return x confined between x_min and x_max inclusive.
+
+    Usage examples:
+        >>> clamp(-1, 0, 9)
+        0
+
+        >>> clamp(1, 0, 9)
+        1
+
+        >>> clamp(10, 0, 9)
+        9
+
+    """
+    return max(min(x, x_max), x_min)
