9300faf29bbe8f34aaf0b99fbf1b29a84b760c49
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2016-05-26 18:58:51 +0100

    mel rotomap-overview: search paths for rotomaps
    This command is easier to use if it searches for rotomap directories for
    itself, as opposed to requiring them to be supplied to it.
    
    Now you can supply the parent directory of multiple rotomaps and it will
    overview all of them.
    
    Test Plan:
    $ ./meta/run_tests.sh
    
    .. In a rotomap data dir ..
    $ mel rotomap-overview .
    LeftArm/Lower/20160117T1757 ++++++++++++++++++++
    LeftArm/Lower/20160117T1759 -------------------
    RightArm/Lower/20160117T1802 +++++++++++++++++++++++++++++
    RightArm/Lower/20160117T1803 +++++++++-+++++++++++

--- py/mel/cmd/rotomapoverview.py
+++ py/mel/cmd/rotomapoverview.py
@@ -1,33 +1,45 @@
 """Display an overview of all the rotomaps at the paths."""
 
 import os
+import re
 
 
 def setup_parser(parser):
     parser.add_argument(
         'PATH',
         nargs='+',
-        help="Path to the rotomap.")
+        help="Path to look for rotomaps.")
 
 
 def process_args(args):
+    rotomap_re = re.compile('\d{8}T\d{4}')
+
     for path in args.PATH:
-        files = os.listdir(path)
+        for root, dirs, files in os.walk(path):
+            if dirs:
+                pass
+            if not files:
+                pass
+            if rotomap_re.fullmatch(os.path.basename(root)):
+                overview = make_overview(root, files)
+                print(root, overview)
 
-        angles = {f: None for f in files if f.lower().endswith('.jpg')}
 
-        for f in files:
-            if f.lower().endswith('.json'):
-                jpg_name = f[:-5]
-                if jpg_name in angles:
-                    angles[jpg_name] = f
+def make_overview(path, files):
+    angles = {f: None for f in files if f.lower().endswith('.jpg')}
 
-        results = []
-        for a in sorted(angles):
-            data_path = angles[a]
-            if data_path is None:
-                results.append('-')
-            else:
-                results.append('+')
+    for f in files:
+        if f.lower().endswith('.json'):
+            jpg_name = f[:-5]
+            if jpg_name in angles:
+                angles[jpg_name] = f
 
-        print(path, ''.join(results))
+    results = []
+    for a in sorted(angles):
+        data_path = angles[a]
+        if data_path is None:
+            results.append('-')
+        else:
+            results.append('+')
+
+    return(''.join(results))
