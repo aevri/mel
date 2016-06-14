541e0e1a3c4d5201591722928dbbcba788ab56d6
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2016-06-14 16:10:29 +0100

    mel micro-compare: side-by-side, more controls
    Display two images side-by-side, allow control over the first image with
    the left and right arrow keys. Use space to swap the left and right
    image.
    
    Test Plan:
    $ ./meta/run_tests.sh
    .. exercise controls of 'mel micro-compare' with existing mole ..

--- py/mel/cmd/microcompare.py
+++ py/mel/cmd/microcompare.py
@@ -53,10 +53,16 @@ def process_args(args):
 
     index = 0
     images = get_comparison_images(args.PATH)
-    if images:
-        display.show_image(images[index])
+    if not images:
+        raise Exception("No microscope images at {}".format(args.PATH))
 
-    print("Press left arrow or right arrow to display another image.")
+    display_images = [images[0], images[-1]]
+
+    montage = mel.lib.image.montage_horizontal(10, *display_images)
+    display.show_image(montage)
+
+    print("Press left arrow or right arrow to change image in the left slot.")
+    print("Press space to swap left slot and right slot.")
     print("Press any other key to exit.")
 
     is_finished = False
@@ -65,9 +71,17 @@ def process_args(args):
         if key != -1:
             if key == mel.lib.ui.WAITKEY_RIGHT_ARROW:
                 index = (index + 1) % len(images)
-                display.show_image(images[index])
+                display_images[0] = images[index]
+                montage = mel.lib.image.montage_horizontal(10, *display_images)
+                display.show_image(montage)
             elif key == mel.lib.ui.WAITKEY_LEFT_ARROW:
                 index = (index + len(images) - 1) % len(images)
-                display.show_image(images[index])
+                display_images[0] = images[index]
+                montage = mel.lib.image.montage_horizontal(10, *display_images)
+                display.show_image(montage)
+            elif key == ord(' '):
+                display_images.reverse()
+                montage = mel.lib.image.montage_horizontal(10, *display_images)
+                display.show_image(montage)
             else:
                 is_finished = True
