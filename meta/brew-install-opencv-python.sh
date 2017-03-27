316f589fb96ab36275b1aeb36acde0d0c52e0b1f
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2017-03-27 21:28:16 +0100

    brew-install-opencv-python.sh: expect Python 3.6
    Homebrew now installs Python 3.6, so expect that instead.

--- meta/brew-install-opencv-python.sh
+++ meta/brew-install-opencv-python.sh
@@ -5,7 +5,7 @@ brew install python3
 brew tap homebrew/science
 brew install opencv3 --with-python3
 echo /usr/local/opt/opencv3/lib/python2.7/site-packages >> /usr/local/lib/python2.7/site-packages/opencv3.pth
-echo /usr/local/opt/opencv3/lib/python3.5/site-packages >> /usr/local/lib/python3.5/site-packages/opencv3.pth
+echo /usr/local/opt/opencv3/lib/python3.6/site-packages >> /usr/local/lib/python3.6/site-packages/opencv3.pth
 echo 'Try "import cv2" ..'
 python3 -c 'import cv2'
 echo OK
