aevri/mel

commit 7cf45ae77c972a29b4d048afdb1dccb3b2fd62ce

Refactor, use pathlib in RotomapDirectory

mel/rotomap/moles.py
@@ -5,6 +5,7 @@ import argparse
 import json
 import math
 import os
+import pathlib
 import uuid
 
 import cv2
@@ -16,15 +17,14 @@ import mel.lib.math
 class RotomapDirectory():
 
     def __init__(self, path):
-        self.path = path
-        if not os.path.isdir(self.path):
+        self.path = pathlib.Path(path)
+        if not self.path.is_dir():
             raise ValueError(
                 '"{}" is not a directory, so not a rotomap.'.format(self.path))
-        files = os.listdir(self.path)
         self.image_paths = [
             os.path.join(self.path, f)
-            for f in files
-            if f.lower().endswith('.jpg')
+            for f in self.path.iterdir()
+            if f.suffix.lower() == '.jpg'
         ]
         if not self.image_paths:
             raise ValueError(

mel/cmd/rotomapudiff.py
@@ -41,7 +41,6 @@ Example output:
 
 import collections
 import itertools
-import os
 
 import mel.rotomap.moles
 
@@ -133,9 +132,9 @@ def process_args(args):
 
 def load_potential_set_file(path, filename):
     ignore_set = set()
-    file_path = os.path.join(path, filename)
-    if os.path.isfile(file_path):
-        with open(file_path) as f:
+    file_path = path / filename
+    if file_path.is_file():
+        with file_path.open() as f:
             lines = f.read().splitlines()
         for l in lines:
             text = l.strip()

commit 91ea88e24490f237287cd9ebac1b60e549c8e45b

Fix 'mel rotomap-edit' startup crash
Fix the bug introduced in 7cf45ae77c972a29b4d048afdb1dccb3b2fd62ce,
where the image paths were accidentally appended to themselves, meaning
that no images were loaded. This resulted in a crash on startup of
'mel rotomap-edit'.

mel/rotomap/moles.py
@@ -21,11 +21,12 @@ class RotomapDirectory():
         if not self.path.is_dir():
             raise ValueError(
                 '"{}" is not a directory, so not a rotomap.'.format(self.path))
+
         self.image_paths = [
-            os.path.join(self.path, f)
-            for f in self.path.iterdir()
+            str(f) for f in self.path.iterdir()
             if f.suffix.lower() == '.jpg'
         ]
+
         if not self.image_paths:
             raise ValueError(
                 '"{}" has no images, so not a rotomap.'.format(self.path))