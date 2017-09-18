787dd462aeb97c86a11a9fbd979d326c4a915ad0
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2017-09-18 10:28:41 +0100

    fixup! WIP: MoleClassifier

--- mel/jupyter/utils.py
+++ mel/jupyter/utils.py
@@ -9,18 +9,21 @@ import scipy.stats
 import mel.lib.moleimaging
 
 
-def frames_to_uuid_poslist(frame_iterable):
-    uuid_to_poslist = collections.defaultdict(list)
+_MAGIC_CLOSE_DISTANCE = 0.1
 
-    for frame in frame_iterable:
-        mask = frame.load_mask()
-        contour = biggest_contour(mask)
-        ellipse = cv2.fitEllipse(contour)
-        elspace = EllipseSpace(ellipse)
-        for uuid, pos in frame.moledata.uuid_points.items():
-            uuid_to_poslist[uuid].append(elspace.to_space(pos))
 
-    return uuid_to_poslist
+def frame_to_uuid_to_pos(frame):
+    mask = frame.load_mask()
+    contour = biggest_contour(mask)
+    ellipse = cv2.fitEllipse(contour)
+    elspace = EllipseSpace(ellipse)
+
+    uuid_to_pos = {
+        uuid_: elspace.to_space(pos)
+        for uuid_, pos in frame.moledata.uuid_points.items()
+    }
+
+    return uuid_to_pos
 
 
 def frames_to_uuid_frameposlist(frame_iterable):
@@ -40,38 +43,12 @@ def frames_to_uuid_frameposlist(frame_iterable):
 
 class MoleClassifier():
 
-    def __init__(self, uuid_to_poslist):
-        self.uuids, self.poslistlist = zip(*uuid_to_poslist.items())
-        yposlistlist = tuple(
-            numpy.array(tuple(y for x, y in poslist))
-            for poslist in self.poslistlist
-        )
-        self.ykernels = tuple(
-            scipy.stats.gaussian_kde(yposlist)
-            for yposlist in yposlistlist
-        )
-
-    def guess_from_ypos(self, ypos):
-        densities = numpy.array(tuple(k(ypos) for k in self.ykernels))
-        total_density = numpy.sum(densities)
-        i = numpy.argmax(densities)
-        d = densities[i][0]
-        return self.uuids[i], d, d / total_density
-
-
-class MoleClassifier2():
-
     def __init__(self, uuid_to_frameposlist):
         uuid_to_poslist = {
             uuid_: [pos for frame, pos in frameposlist]
             for uuid_, frameposlist in uuid_to_frameposlist.items()
         }
-        # self.uuids, self.frameposlistlist = zip(*uuid_to_frameposlist.items())
         self.uuids, self.poslistlist = zip(*uuid_to_poslist.items())
-        # self.poslistlist = tuple(
-        #     [pos for frame, pos in frameposlist]
-        #     for frameposlist in self.frameposlistlist
-        # )
         yposlistlist = tuple(
             numpy.array(tuple(y for x, y in poslist))
             for poslist in self.poslistlist
@@ -81,13 +58,84 @@ class MoleClassifier2():
             for yposlist in yposlistlist
         )
 
-    def guess_from_ypos(self, ypos):
-        # TODO: check that ypos is scalar
-        densities = numpy.array(tuple(k(ypos) for k in self.ykernels))
+        self.frames = collections.defaultdict(dict)
+        for uuid_, frameposlist in uuid_to_frameposlist.items():
+            for frame, pos in frameposlist:
+                self.frames[frame][uuid_] = pos
+
+        uuid_to_neighbourlist = collections.defaultdict(list)
+        for uuid_to_pos in self.frames.values():
+            for uuid_, num_close in uuidtopos_to_numclose(uuid_to_pos).items():
+                uuid_to_neighbourlist[uuid_].append(num_close)
+
+        self.uuid_to_neighbourlist = uuid_to_neighbourlist
+
+        self.numclose_kernels = tuple(
+            scipy.stats.gaussian_kde(
+                numpy.array(
+                    uuid_to_neighbourlist[uuid_]))
+            for uuid_ in self.uuids
+        )
+
+    def guesses_from_ypos(self, ypos):
+
+        if not numpy.isscalar(ypos):
+            raise ValueError(f"'ypos' must be a scalar, got '{ypos}'.")
+
+        densities = numpy.array(tuple(k(ypos)[0] for k in self.ykernels))
         total_density = numpy.sum(densities)
-        i = numpy.argmax(densities)
-        d = densities[i][0]
-        return self.uuids[i], d, d / total_density
+
+        if numpy.isclose(total_density, 0):
+            return []
+
+        matches = []
+        for i, m_uuid in enumerate(self.uuids):
+            p = densities[i]
+            q = p / total_density
+            matches.append((m_uuid, p, q))
+
+        return matches
+
+    def guesses_from_neighbours(self, uuid_pos):
+
+        # Calculate neighbour values for all supplied moles
+        uuid_to_numclose = uuidtopos_to_numclose(uuid_pos)
+
+        # Calculate probability for all supplied moles vs. all self moles
+        matches = []
+        for uuid_, numclose in uuid_to_numclose.items():
+            densities = numpy.array(tuple(
+                k(numclose)[0] for k in self.numclose_kernels))
+            total_density = numpy.sum(densities)
+
+            if numpy.isclose(total_density, 0):
+                continue
+
+            # i = numpy.argmax(densities)
+            # d = densities[i][0]
+            for i, m_uuid in enumerate(self.uuids):
+                p = densities[i]
+                q = p / total_density
+                matches.append((uuid_, m_uuid, p, q))
+
+        return matches
+
+
+def uuidtopos_to_numclose(uuid_to_pos):
+    uuid_to_numclose = {}
+    for uuid_, pos in uuid_to_pos.items():
+        distances = [
+            numpy.linalg.norm(pos - n_pos)
+            for n_uuid, n_pos in uuid_to_pos.items()
+            if n_uuid != uuid_
+        ]
+        if distances:
+            num_close = sum(
+                1 / ((m / _MAGIC_CLOSE_DISTANCE) + 1)
+                for m in distances
+            )
+            uuid_to_numclose[uuid_] = num_close
+    return uuid_to_numclose
 
 
 def biggest_contour(mask):
@@ -171,7 +219,7 @@ def from_ellipse_space(ellipse, pos):
         int(right[1] * pos[0] * rmag + up[1] * pos[1] * umag + center[1]),
     )
 
-    return p
+    return numpy.array(p)
 
 
 def to_ellipse_space(ellipse, pos):
@@ -187,7 +235,7 @@ def to_ellipse_space(ellipse, pos):
         pos[0] * up[0] + pos[1] * up[1],
     )
 
-    return (
+    return numpy.array((
         pos[0] / rmag,
         pos[1] / umag,
-    )
+    ))
