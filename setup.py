60ce0aa7d0bbd0290e468830fa37de15bb9277e0
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2018-03-05 17:36:57 +0000

    setup.py: avoid pkg_resources import
    Make mel faster to start up by avoiding the import of pkg_resources in
    the generated launcher script.

--- setup.py
+++ setup.py
@@ -1,4 +1,38 @@
-import setuptools
+import setuptools.command.easy_install
+
+# -----------------------------------------------------------------------------
+# Monkey-patch setuptools for performance, to avoid importing pkg_resources.
+#
+# This workaround is copied from BuildStream:
+# https://gitlab.com/BuildStream/buildstream/blob/master/setup.py#L143
+#
+# That solution was inspired by https://github.com/ninjaaron/fast-entry_points
+# which we believe was also inspired from the code from `setuptools` project.
+#
+TEMPLATE = '''\
+# -*- coding: utf-8 -*-
+import sys
+
+from {0} import {1}
+
+if __name__ == '__main__':
+    sys.exit({2}())'''
+
+
+@classmethod
+def get_args(cls, dist, header=None):
+    if header is None:
+        header = cls.get_header()
+    for name, ep in dist.get_entry_map('console_scripts').items():
+        cls._ensure_safe_name(name)
+        script_text = TEMPLATE.format(ep.module_name, ep.attrs[0], '.'.join(ep.attrs))
+        args = cls._get_script_args('console', name, header, script_text)
+        for res in args:
+            yield res
+
+
+setuptools.command.easy_install.ScriptWriter.get_args = get_args
+# -----------------------------------------------------------------------------
 
 lowerbound = setuptools.Extension(
     'mel.rotomap.lowerbound',
