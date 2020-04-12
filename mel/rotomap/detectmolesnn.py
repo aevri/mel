79aef2c878cbdd8b00e901c3dc21e11a987defc1
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2020-04-12 13:09:55 +0100

    WIP: make non moles 5x more likely

--- mel/rotomap/detectmolesnn.py
+++ mel/rotomap/detectmolesnn.py
@@ -458,7 +458,7 @@ def int_tensor2d_to_tuple(tensor2d):
 
 
 def reduce_nonmole_locations(
-    locations, mole_locations, min_sq_dist=64 * 64, non_mole_chance=0.001
+    locations, mole_locations, min_sq_dist=64 * 64, non_mole_chance=0.005
 ):
     if not mole_locations:
         near_moles = torch.tensor([False] * len(locations))
