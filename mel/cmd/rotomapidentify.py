c55a8c395c8d016c0fed911830df82542c14c2ba
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2020-12-29 11:24:50 +0000

    rotomapidentify: mv reusable bits to identifynn

--- mel/cmd/rotomapidentify.py
+++ mel/cmd/rotomapidentify.py
@@ -1,10 +1,6 @@
 """Guess which mole is which in a rotomap image."""
 
-import collections
-import json
 import os
-
-import mel.lib.fs
 
 
 def setup_parser(parser):
@@ -22,7 +18,10 @@ def setup_parser(parser):
 
 
 def process_args(args):
-    identifier = make_identifier()
+    # This is an expensive import, so only do it when necessary.
+    import mel.rotomap.identifynn
+
+    identifier = mel.rotomap.identifynn.make_identifier()
     for target in args.TARGET:
         if args.verbose:
             print("Processing", target, "..")
@@ -33,82 +32,6 @@ def process_args(args):
         new_moles = identifier.get_new_moles(frame)
 
         mel.rotomap.moles.save_image_moles(new_moles, str(frame.path))
-
-
-def make_identifier():
-    melroot = mel.lib.fs.find_melroot()
-    model_dir = melroot / mel.lib.fs.DEFAULT_CLASSIFIER_PATH
-    model_path = model_dir / "identify.pth"
-    metadata_path = model_dir / "identify.json"
-    return MoleIdentifier(metadata_path, model_path)
-
-
-class MoleIdentifier:
-    def __init__(self, metadata_path, model_path):
-        # Some of these imports are expensive, so to keep program start-up time
-        # lower, import them only when necessary.
-        import torch
-        import mel.rotomap.identifynn
-
-        with open(metadata_path) as f:
-            self.metadata = json.load(f)
-
-        model_args = self.metadata["model_args"]
-        self.part_to_index = self.metadata["part_to_index"]
-        self.classes = self.metadata["classes"]
-        self.class_to_index = {cls: i for i, cls in enumerate(self.classes)}
-
-        self.in_fields = ["part_index"]
-        self.in_fields.extend(["molemap", "molemap_detail_2", "molemap_detail_4"])
-        self.out_fields = ["uuid_index", "mole_count"]
-
-        self.model = mel.rotomap.identifynn.Model(**model_args)
-        self.model.load_state_dict(torch.load(model_path))
-
-    def get_new_moles(self, frame):
-        import torch
-        import torch.utils.data
-
-        import mel.rotomap.identifynn
-        class_to_index2 = self.class_to_index.copy()
-        for m in frame.moles:
-            uuid_ = m["uuid"]
-            if uuid_ not in class_to_index2:
-                class_to_index2[uuid_] = -1
-
-        datadict = collections.defaultdict(list)
-        mel.rotomap.identifynn.extend_dataset_by_frame(
-            dataset=datadict,
-            frame=frame,
-            image_size=self.metadata["image_size"],
-            part_to_index=self.part_to_index,
-            do_channels=False,
-            channel_cache=None,
-            class_to_index=class_to_index2,
-            escale=1.0,
-            etranslate=0.0,
-        )
-
-        dataset = mel.rotomap.identifynn.RotomapsDataset(
-            datadict,
-            classes=self.classes,
-            class_to_index=class_to_index2,
-            in_fields=self.in_fields,
-            out_fields=self.out_fields,
-        )
-
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=1)
-
-        new_moles = list(frame.moles)
-        self.model.eval()
-        with torch.no_grad():
-            for i, xb, _ in dataloader:
-                if new_moles[i][mel.rotomap.moles.KEY_IS_CONFIRMED]:
-                    continue
-                out = self.model(xb)
-                preds = torch.argmax(out[0], dim=1)
-                new_moles[i]["uuid"] = self.classes[preds]
-        return new_moles
 
 
 # -----------------------------------------------------------------------------
--- mel/rotomap/identifynn.py
+++ mel/rotomap/identifynn.py
@@ -11,6 +11,85 @@ import tqdm
 
 import mel.lib.ellipsespace
 import mel.rotomap.moles
+import json
+import mel.lib.fs
+import mel.rotomap.identifynn
+
+
+def make_identifier():
+    melroot = mel.lib.fs.find_melroot()
+    model_dir = melroot / mel.lib.fs.DEFAULT_CLASSIFIER_PATH
+    model_path = model_dir / "identify.pth"
+    metadata_path = model_dir / "identify.json"
+    return mel.rotomap.identifynn.MoleIdentifier(metadata_path, model_path)
+
+
+class MoleIdentifier:
+    def __init__(self, metadata_path, model_path):
+        # Some of these imports are expensive, so to keep program start-up time
+        # lower, import them only when necessary.
+        import torch
+        import mel.rotomap.identifynn
+
+        with open(metadata_path) as f:
+            self.metadata = json.load(f)
+
+        model_args = self.metadata["model_args"]
+        self.part_to_index = self.metadata["part_to_index"]
+        self.classes = self.metadata["classes"]
+        self.class_to_index = {cls: i for i, cls in enumerate(self.classes)}
+
+        self.in_fields = ["part_index"]
+        self.in_fields.extend(["molemap", "molemap_detail_2", "molemap_detail_4"])
+        self.out_fields = ["uuid_index", "mole_count"]
+
+        self.model = mel.rotomap.identifynn.Model(**model_args)
+        self.model.load_state_dict(torch.load(model_path))
+
+    def get_new_moles(self, frame):
+        import torch
+        import torch.utils.data
+
+        import mel.rotomap.identifynn
+        class_to_index2 = self.class_to_index.copy()
+        for m in frame.moles:
+            uuid_ = m["uuid"]
+            if uuid_ not in class_to_index2:
+                class_to_index2[uuid_] = -1
+
+        datadict = collections.defaultdict(list)
+        mel.rotomap.identifynn.extend_dataset_by_frame(
+            dataset=datadict,
+            frame=frame,
+            image_size=self.metadata["image_size"],
+            part_to_index=self.part_to_index,
+            do_channels=False,
+            channel_cache=None,
+            class_to_index=class_to_index2,
+            escale=1.0,
+            etranslate=0.0,
+        )
+
+        dataset = mel.rotomap.identifynn.RotomapsDataset(
+            datadict,
+            classes=self.classes,
+            class_to_index=class_to_index2,
+            in_fields=self.in_fields,
+            out_fields=self.out_fields,
+        )
+
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=1)
+
+        new_moles = list(frame.moles)
+        self.model.eval()
+        with torch.no_grad():
+            for i, xb, _ in dataloader:
+                if new_moles[i][mel.rotomap.moles.KEY_IS_CONFIRMED]:
+                    continue
+                out = self.model(xb)
+                preds = torch.argmax(out[0], dim=1)
+                new_moles[i]["uuid"] = self.classes[preds]
+        return new_moles
 
 
 def fit(
