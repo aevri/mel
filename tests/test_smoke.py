41234ad46c000ee479dd64eede4921a038895708
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2022-06-19 22:40:02 +0400

    rotomap compare-extra-stem: check performance
    Make it possible to compare the data in an 'extra-stem' namespace with
    the canonical data in the main namespace.
    
    This includes printing the precision and recall vs. the 'ground truth'
    in the main namespace, as well as the raw stats.
    
    For example it can be run like this:
    
        $ find rotomaps/parts/{Left,Right}*/{Upper,Lower}/2021_* -iname '*.jpg' | xargs mel r compare-extra-stem --error-distance 10 auto
        matched: 2037
        missing: 841
        added: 437
        Recall: 71%
        Precision: 82%

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -103,6 +103,9 @@ def test_smoke():
             *target_image_files
         )
         expect_ok(
+            "mel", "rotomap", "compare-extra-stem", "smoke", *target_image_files
+        )
+        expect_ok(
             "mel", "rotomap", "merge-extra-stem", "smoke", *target_image_files
         )
         expect_ok("mel", "rotomap", "identify-train", "--extra-stem", "smoke")
