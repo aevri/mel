35e2937356f1464a11eda40d2cf9f27ee6ad9907
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2023-01-06 07:57:41 +0000

    mel.lib.fs: list_rotomaps_by_part, list_rotomap_images_by_session
    These come in very handy for constructing training and validation
    datasets.

--- mel/lib/fs.py
+++ mel/lib/fs.py
@@ -1,5 +1,6 @@
 """FileSystem helpers."""
 
+import collections
 import os
 import pathlib
 
@@ -52,8 +53,34 @@ def find_melroot():
         path = parent
 
 
+def list_rotomaps_by_part(parts_path):
+    all_rotomaps = collections.defaultdict(list)
+    for part in parts_path.iterdir():
+        for subpart in part.iterdir():
+            subpart_paths = sorted(p for p in subpart.iterdir())
+            for p in subpart_paths:
+                all_rotomaps[f"{part.stem}/{subpart.stem}"].append(p)
+    return all_rotomaps
+
+
+def list_rotomap_images_by_session(parts_path, *, exclude_parts=None):
+    images = collections.defaultdict(list)
+    for part in sorted(parts_path.iterdir()):
+        for subpart in sorted(part.iterdir()):
+            if (
+                exclude_parts
+                and f"{part.stem}/{subpart.stem}" in exclude_parts
+            ):
+                continue
+            for session in sorted(subpart.iterdir()):
+                files = sorted(yield_only_jpegs_from_dir(session))
+                for img in files:
+                    images[f"{session.stem}"].append(img)
+    return images
+
+
 # -----------------------------------------------------------------------------
-# Copyright (C) 2018 Angelos Evripiotis.
+# Copyright (C) 2018-2023 Angelos Evripiotis.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
