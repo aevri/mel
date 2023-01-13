97372b5955103ce552368c4e7aefc1f4bbcfc486
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2023-01-13 13:17:16 +0000

    vulture: ignore PyTorch lightning module methods
    These methods appear as unused to Vulture, but they are called by
    PyTorch lightning during training of the model.

--- meta/static_tests.sh
+++ meta/static_tests.sh
@@ -15,7 +15,10 @@ printf '.'
 python3 -m pyflakes $allscripts
 
 printf '.'
-python3 -m vulture --exclude '*__t.py,mel/rotomap/detectmoles.py,mel/rotomap/identifynn.py' mel/
+python3 -m vulture \
+    --ignore-names training_step,validation_step,configure_optimizers \
+    --exclude '*__t.py,mel/rotomap/detectmoles.py,mel/rotomap/identifynn.py' \
+    mel/
 
 echo OK
 trap - EXIT
