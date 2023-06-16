e081207ebe698a7fe88a70fd9e7795c13fd65fb3
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2023-06-16 18:09:07 +0100

    rotomap/identifynn2: copyright

--- mel/rotomap/identifynn2.py
+++ mel/rotomap/identifynn2.py
@@ -1,6 +1,5 @@
 """Identify moles from their positions in images."""
 
-
 import torch
 
 
@@ -86,3 +85,20 @@ class Model(torch.nn.Module):
         # Minimal test version: just a linear layers looking at the self_pos.
 
         pass
+
+
+# -----------------------------------------------------------------------------
+# Copyright (C) 2023 Angelos Evripiotis.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------ END-OF-FILE ----------------------------------
