e024a7085e21b216965153a34d4774fd418bd2cf
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2023-06-30 18:05:56 +0100

    rotomapidentify2train: print validation metrics

--- mel/cmd/rotomapidentify2train.py
+++ mel/cmd/rotomapidentify2train.py
@@ -103,6 +103,9 @@ def process_args(args):
         trainer.train(10)
         trainer.validate()
 
+    print("Validation loss:", trainer.valid_loss[-1])
+    print("Validation acc:", f"{trainer.valid_acc[-1]:.0%}")
+
     torch.save(model.state_dict(), model_path)
     print(f"Saved {model_path}")
 
