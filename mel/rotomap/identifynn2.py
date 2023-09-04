fe442621e923dd400d2f96f48096230a91fe1311
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2023-09-04 19:25:34 +0100

    identifynn2: refactor, rm pool

--- mel/rotomap/identifynn2.py
+++ mel/rotomap/identifynn2.py
@@ -313,7 +313,6 @@ class PosModel(torch.nn.Module):
         self.transformer = torch.nn.TransformerEncoder(
             transformer_layer, num_layers=2
         )
-        self.pool = torch.nn.AdaptiveMaxPool1d(1)
 
     def freeze_except_classifier(self):
         for sub in [
@@ -376,22 +375,12 @@ class PosModel(torch.nn.Module):
             emb_sequence
         )  # shape: (batch_size, sequence_length, embed_dim)
 
-        # pooled_output = self.pool(transformer_output.permute(0, 2, 1)).squeeze(
-        #     -1
-        # )  # shape: (batch_size, embed_dim)
-
-        pooled_output = transformer_output[:, 0, :]
-
-        # return pooled_output
-
-        # transformer_output_flat = transformer_output.view(
-        #     transformer_output.size(0), -1
-        # )  # shape: (batch_size, sequence_length * embed_dim)
+        item_output = transformer_output[:, 0, :]
 
         return torch.cat(
             [
                 selfpos_emb,
-                pooled_output.reshape(pooled_output.size(0), -1),
+                item_output.reshape(item_output.size(0), -1),
                 partname_embedding,
             ],
             dim=-1,
