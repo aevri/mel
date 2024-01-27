ff0bb8563815e267cce90801520890511bc851f3
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2024-01-27 22:16:32 +0000

    test_merge_in_radiuses_happy_merge_extra

--- tests/rotomap/test_automark.py
+++ tests/rotomap/test_automark.py
@@ -33,33 +33,21 @@ def test_merge_in_radiuses_happy(only_merge, error_distance):
     assert result[2]["radius"] == 18
 
 
-def test_merge_in_radiuses_happy_only_merge():
-    error_distance = 3
-    only_merge = True
-
+@pytest.mark.parametrize("only_merge", [True, False])
+@pytest.mark.parametrize("error_distance", [0, 1, 2, 3, 4, 5])
+def test_merge_in_radiuses_happy_merge_extra(only_merge, error_distance):
     result = automark.merge_in_radiuses(TARGETS, RADII_SOURCES, error_distance, only_merge)
 
-    assert len(result) == 3
+    if only_merge:
+        assert len(result) == 3
+    else:
+        assert len(result) == 4
     assert result[0]["uuid"] == "1"
     assert result[0]["radius"] == 7
     assert result[1]["uuid"] == "2"
     assert result[1]["radius"] == 12
     assert result[2]["uuid"] == "3"
     assert result[2]["radius"] == 18
-
-
-def test_merge_in_radiuses_happy_not_only_merge():
-    error_distance = 3
-    only_merge = False
-
-    result = automark.merge_in_radiuses(TARGETS, RADII_SOURCES, error_distance, only_merge)
-
-    assert len(result) == 4
-    assert result[0]["uuid"] == "1"
-    assert result[0]["radius"] == 7
-    assert result[1]["uuid"] == "2"
-    assert result[1]["radius"] == 12
-    assert result[2]["uuid"] == "3"
-    assert result[2]["radius"] == 18
-    assert result[3]["uuid"] == "7"
-    assert result[3]["radius"] == 24
+    if not only_merge:
+        assert result[3]["uuid"] == "7"
+        assert result[3]["radius"] == 24
